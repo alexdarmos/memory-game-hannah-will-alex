ea4f652eab1c686f5c6854300374f872a93a2274
Will Morrissey <wtm.41691@gmail.com>
2019-03-20 21:23:18 -0400

    Added possible JS function for button flip

---
+++ main.js
@@ -0,0 +1,36 @@
+//document ready jquery
+$(() => {
+
+let totalSeconds = 0;
+let totalMinutes =0;
+let interval = setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSeconds;
+    $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+    $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+
+    //temporarily stops timer at 5 seconds for my sanity
+    if (totalSeconds === 59) {
+        totalMinutes += 1;
+        totalSeconds = 0;
+    }
+};
+})
+
+
+
+// Possible implementation for button flipping - Still requires CSS
+$('#flipto').on("click", function(event) {
+  event.preventDefault();
+
+  var face = $(this).data("face");
+
+  if ( face == "bottom" ) {
+    $(".cube").removeClass("flip-to-top").addClass("flip-to-bottom");
+    $(this).data("face", "top").text("Flip: to top");
+  } else {
+    $(".cube").removeClass("flip-to-bottom").addClass("flip-to-top");
+    $(this).data("face", "bottom").text("Flip: to bottom");
+  }
+});

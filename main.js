70e8c537447762276ebd836c948f73e05d03f1a9
hannah barker <hannah.m.barker@gmail.com>
2019-03-21 14:02:32 -0400

    alex jquery add

--- main.js
+++ main.js
@@ -1,18 +1,65 @@
 
+//     function play(){
+//         var audio = document.getElementById("audio");
+//         audio.play();
+
+//         // var audio2 = document.getElementById("audio-2");
+//         // audio2.play();
+       
+//                   }
+
+
+// $(() => {
+//     $('.flip-card').click(function() {
+//         $(this).toggleClass('active');
+//     });
+
+
+// });
+
+
+//document ready jquery
+$(() => {
+    $('.flip-card').click(function() {
+        $(this).toggleClass('active');
+    });
+
+    let totalSeconds = 0;
+    let totalMinutes =0;
+    let interval = setInterval(setTime, 1000);
+    
+    function setTime() {
+        ++totalSeconds;
+        $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+        $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+    
+        //temporarily stops timer at 5 seconds for my sanity
+        if (totalSeconds === 59) {
+            totalMinutes += 1;
+            totalSeconds = 0;
+        }
+    };
+    });
+    
     function play(){
         var audio = document.getElementById("audio");
         audio.play();
 
         // var audio2 = document.getElementById("audio-2");
         // audio2.play();
-       
-                  }
-
-
-$(() => {
-    $('.flip-card').click(function() {
-        $(this).toggleClass('active');
-    });
-
-
-});
+    
+        }
+    // Possible implementation for button flipping - Still requires CSS
+    // $('#flipto').on("click", function(event) {
+    //   event.preventDefault();
+    
+    //   var face = $(this).data("face");
+    
+    //   if ( face == "bottom" ) {
+    //     $(".cube").removeClass("flip-to-top").addClass("flip-to-bottom");
+    //     $(this).data("face", "top").text("Flip: to top");
+    //   } else {
+    //     $(".cube").removeClass("flip-to-bottom").addClass("flip-to-top");
+    //     $(this).data("face", "bottom").text("Flip: to bottom");
+    //   }
+  

8db35ddd282fdcd7b71acd5cd273656d7dc7f893
unknown <alex.darmos@gmail.com>
2019-03-20 21:11:08 -0400

    updated timer and displayed on page

--- main.js
+++ main.js
@@ -2,15 +2,22 @@
 $(() => {
 
 let totalSeconds = 0;
+let totalMinutes =0;
 let interval = setInterval(setTime, 1000);
 
 function setTime() {
     ++totalSeconds;
-    console.log(totalSeconds);
+    $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+    $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+
     //temporarily stops timer at 5 seconds for my sanity
-    if (totalSeconds === 5) {
-        clearInterval(interval);
+    if (totalSeconds === 59) {
+        totalMinutes += 1;
+        totalSeconds = 0;
+
     }
 };
+
+
 
 })

d7f6132010ba90704aed75824a44fbf9df9a03f5
unknown <alex.darmos@gmail.com>
2019-03-21 18:53:21 -0400

    reset button added

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 //document ready jquery
 $(() => {
+    let totalSeconds = 0;
+    let totalMinutes =0;
+    let interval;
+    // let interval = setInterval(setTime, 1000);
 
-let totalSeconds = 0;
-let totalMinutes =0;
-let interval = setInterval(setTime, 1000);
 
-function setTime() {
-    ++totalSeconds;
-    $(`.seconds`).text(`Seconds: ${totalSeconds}`);
-    $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+    // function stopTime(intervalParam) {
+    //     clearInterval(intervalParam);
+    // }
 
-    //temporarily stops timer at 5 seconds for my sanity
-    if (totalSeconds === 59) {
-        totalMinutes += 1;
+    function setTime() {
+        ++totalSeconds;
+        $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+        $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+
+        //temporarily stops timer at 5 seconds for my sanity
+        if (totalSeconds === 59) {
+            totalMinutes += 1;
+            totalSeconds = 0;
+        }
+
+    };
+
+
+    $(`#Start`).on(`click`, (e) => {
+        console.log(e); 
+        // setTime();
+        interval = setInterval(setTime, 1000);
+    })
+
+    $(`#Reset`).on(`click`, (e) => {
+        console.log(e);
+        clearInterval(interval);
         totalSeconds = 0;
-    }
-};
+        totalMinutes = 0;
+
+        
+    });
+
+
+    
+
+    
 })

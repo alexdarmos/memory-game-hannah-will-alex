b687013b251868aab8be33c002aa8393840f2247
unknown <alex.darmos@gmail.com>
2019-03-21 20:17:05 -0400

    reset timer working

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 //document ready jquery
 $(() => {
+    let totalSeconds = 0;
+    let totalMinutes =0;
+    let interval;
+
+    //sets timer in the DOM
+    function domTimer() {
+        $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+        $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+    }
+
+    //increments time
+    function setTime() {
+        ++totalSeconds;
+        //temporarily stops timer at 5 seconds for my sanity
+        if (totalSeconds === 59) {
+            totalMinutes += 1;
+            totalSeconds = 0;
+        }
+        domTimer();  
+    };
+
+    //start game
+    $(`#Start`).on(`click`, (e) => {
+        console.log(`Game Started`); 
+
+        //start timer
+        interval = setInterval(setTime, 1000);
+    })
+
+    //reset game
+    $(`#Reset`).on(`click`, (e) => {
+        console.log(`Game Reset`);
+        //pause timer
+        clearInterval(interval);
+        //reset timer
+        totalSeconds = 0;
+        totalMinutes = 0; 
+        //reset timer in dom
+        domTimer();  
+
+    });
 
     $(`.flip-card-inner`).on(`click`, (e) => {
         var audio = document.getElementById("audio");
@@ -13,43 +54,6 @@ $(() => {
     //     // var audio2 = document.getElementById("audio-2");
     //     // audio2.play();
     //     }
-
-    let totalSeconds = 0;
-    let totalMinutes =0;
-    let interval;
-    // let interval = setInterval(setTime, 1000);
-
-
-    // function stopTime(intervalParam) {
-    //     clearInterval(intervalParam);
-    // }
-
-    function setTime() {
-        ++totalSeconds;
-        $(`.seconds`).text(`Seconds: ${totalSeconds}`);
-        $(`.minutes`).text(`Minutes: ${totalMinutes}`);
-
-        //temporarily stops timer at 5 seconds for my sanity
-        if (totalSeconds === 59) {
-            totalMinutes += 1;
-            totalSeconds = 0;
-        }
-
-    };
-
-
-    $(`#Start`).on(`click`, (e) => {
-        console.log(e); 
-        // setTime();
-        interval = setInterval(setTime, 1000);
-    })
-
-    $(`#Reset`).on(`click`, (e) => {
-        console.log(e);
-        clearInterval(interval);
-        totalSeconds = 0;
-        totalMinutes = 0; 
-    });
 
     $('.flip-card').click(function() {
         $(this).toggleClass('active');

c680b30275f2cc536923857e1f2a4ad947c0fb94
Will Morrissey <wtm.41691@gmail.com>
2019-03-25 20:46:34 -0400

    Updates to JS,HTML, and CSS

--- main.js
+++ main.js
@@ -37,20 +37,35 @@ $('#flipto').on("click", function(event) {
         $(this).toggleClass('active');
     });
 
+    // function play(){
+    //     // var audio = document.getElementById("audio");
+    //     // audio.play();
+
+    //     // var audio2 = document.getElementById("audio-2");
+    //     // audio2.play();
+    //     }
+
     let totalSeconds = 0;
     let totalMinutes =0;
-    let interval = setInterval(setTime, 1000);
-    
-    function setTime() {
-        ++totalSeconds;
+    let interval;
+    let clickCount = 0;
+    let cardOne, cardTwo;
+
+    //sets timer in the DOM
+    function domTimer() {
         $(`.seconds`).text(`Seconds: ${totalSeconds}`);
         $(`.minutes`).text(`Minutes: ${totalMinutes}`);
-    
+    };
+
+    //increments time
+    function setTime() {
+        ++totalSeconds;
         //temporarily stops timer at 5 seconds for my sanity
         if (totalSeconds === 59) {
             totalMinutes += 1;
             totalSeconds = 0;
         }
+
     };
     
     
@@ -58,14 +73,81 @@ $('#flipto').on("click", function(event) {
         var audio = document.getElementById("audio");
         audio.play();
 
-        // var audio2 = document.getElementById("audio-2");
-        // audio2.play();
-    
+
+        domTimer();  
+    };
+
+    //works on dummy cards-currently not in html
+    function compareCards(e) {
+        //track number of clicks
+        ++clickCount;
+        //limit click count to 2
+        while(clickCount > 2) {
+            clickCount = 0;
+            ++clickCount;
+        }
+        // console.log(clickCount);
+
+        if(clickCount === 1) {
+            cardOne = e.target.innerText;
+            console.log(cardOne);
+        } else {
+            cardTwo = e.target.innerText;
+            console.log(cardTwo);
         }
 
+        if(clickCount === 2) {
+            if(cardOne === cardTwo) {
+                console.log(`Match!`);
+            } else {
+                console.log(`Mismatch!`);
+            }
+        }
+    };
 
+    //start game
+    $(`#Start`).on(`click`, (e) => {
+        console.log(`Game Started`); 
 
+        //start timer
+        interval = setInterval(setTime, 1000);
+    })
 
+    //reset game
+    $(`#Reset`).on(`click`, (e) => {
+        console.log(`Game Reset`);
+        //pause timer
+        clearInterval(interval);
+        //reset timer
+        totalSeconds = 0;
+        totalMinutes = 0; 
+        //reset timer in dom
+        domTimer();  
+
+    });
+
+    
+    $(`.flip-card`).on(`click`, (e) => {
+        //compare cards on click
+        compareCards(e);
+        var audio = document.getElementById("audio");
+        audio.play();
+        console.log(e);
+    });
+
+    // function play(){
+    //     // var audio = document.getElementById("audio");
+    //     // audio.play();
+
+    //     // var audio2 = document.getElementById("audio-2");
+    //     // audio2.play();
+    //     }
+
+    $('.flip-card').click(function() {
+        $(this).toggleClass('active');
+    });
+ 
+    
     // Possible implementation for button flipping - Still requires CSS
     // $('#flipto').on("click", function(event) {
     //   event.preventDefault();
@@ -80,3 +162,5 @@ $('#flipto').on("click", function(event) {
     //     $(this).data("face", "bottom").text("Flip: to bottom");
     //   }
   
+
+

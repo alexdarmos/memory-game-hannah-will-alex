ebecba449a4d797da972b945a044e9d7f0c11237
hannah barker <hannah.m.barker@gmail.com>
2019-03-29 22:26:05 -0400

    worked on styling and commenting steps. Removed background opacity shape and added falcon curser

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 //document ready jquery
 $(() => {
+    let totalSeconds = 0;
+    let totalMinutes =0;
+    let interval, cardOne, cardTwo, selectedCardOne, selectedCardTwo;
+    let clickCount = 0;
+    let countDown = 5;
     
-        $(`.flip-card-inner`).on(`click`, (e) => {
+    $(`.flip-card-inner`).on(`click`, (e) => {
         var audio = document.getElementById("audio");
         audio.play();
         
     });
     
 
-    let totalSeconds = 0;
-    let totalMinutes =0;
-    let interval, cardOne, cardTwo;
-    let clickCount = 0;
-    let countDown = 30;
-
     //sets timer in the DOM
     function domTimer() {
-        $(`.seconds`).text(`Seconds: ${totalSeconds}`);
-        $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+        // $(`.seconds`).text(`Seconds: ${totalSeconds}`);
+        // $(`.minutes`).text(`Minutes: ${totalMinutes}`);
+        $(`.timer`).text(`Timer: ${totalMinutes}:${totalSeconds}`);
     };
+
+    //sets countdown timer in DOM
+    function domCountDown() {
+        $(`.timer`).text(`Game Begins in: ${countDown} Seconds`);
+    }
 
     //Keeps track of time it takes user to complete game
     function setTime() {
@@ -37,16 +42,20 @@ $(() => {
         console.log(`Start Game`);
         //start in-game timer
         interval = setInterval(setTime, 1000);
+        //flip cards back to hidden
+        flipCards($('.flip-card'));
+
     };
 
     //displays the count down timer before game actually starts
     function delayTimer() {
         if (countDown != 0) {
             --countDown;
-            console.log(`Count Down: ${countDown}`);
+            // console.log(`Count Down: ${countDown}`);
+            
         } else {
             return;
-        }
+        }domCountDown();
     };
 
     //compares cards based on the src, finds correctly matching cards, **need way to stop user from selecting same card twice** 
@@ -62,29 +71,74 @@ $(() => {
         if(clickCount === 1) {
             cardOne = e.currentTarget.children[2].lastElementChild.src;
             console.log(cardOne);
+            console.log(e.delegateTarget.offsetParent);
+            selectedCardOne = e.delegateTarget.offsetParent;
         } else {
+            
             cardTwo = e.currentTarget.children[2].lastElementChild.src;
+            selectedCardTwo = e.delegateTarget.offsetParent;
             console.log(cardTwo);
         }
 
-        if(clickCount === 2) {
-            if(cardOne === cardTwo) {
-                console.log(`Match!`);
-            } else {
-                console.log(`Mismatch!`);
+        if( selectedCardOne != selectedCardTwo) {
+            if(clickCount === 2) {
+                if(cardOne === cardTwo) {
+                    console.log(`Match!`);
+                } else {
+                    console.log(`Mismatch!`);
+                }
             }
-        }
+    }   else {
+        return;
+    }
     };
+
+    //function to flip all cards at beginning
+    function flipCards(target) {
+        $(target).toggleClass('active');
+    };
+
+    function shuffle(array) {
+        let length = array.length;
+        let random;
+        let index;
+    
+    // While there are elements in the array
+        while (length > 0) {
+    // Pick a random index
+            index = Math.floor(Math.random() * length);
+    // Decrease length by 1
+            length--;
+    // Swap the array elements 
+            random = array[length];
+            array[length] = array[index];
+            array[index] = random;
+        }
+        //Return a random element
+        return random;
+    }
+
+    //event listener to flip card animation on click
+    $('.flip-card').click(function() {
+        $(this).toggleClass('active');
+    });
+
 
     //start game
     $(`#Start`).on(`click`, (e) => {
         console.log(`Game Starting, 30 second delay started`); 
         //function to delay start of game by 30 seconds
-        setTimeout(delayStart, 30000);
+        setTimeout(delayStart, 5000);
         //Countdown 30 second timer before game start
         setInterval(delayTimer, 1000);
         $('.card-container').css('display', 'flex');
         $('.opening-video').css('display', 'none');
+        $('.Start-Reset').css('justify-content', 'center');
+        $('.Start-Reset').css('margin-left', '0px');
+
+
+        flipCards($('.flip-card'));
+
         
     });
 
@@ -119,9 +173,7 @@ $(() => {
     //     }
 
     //event listener to flip card animation
-    $('.flip-card').click(function() {
-        $(this).toggleClass('active');
-    });
+    
  
     
     // Possible implementation for button flipping - Still requires CSS
@@ -162,5 +214,10 @@ $(() => {
     
 //     });    
   
+var div = document.getElementById('falcon');
+			document.addEventListener('mousemove',function(e) {			
+				div.style.left = e.pageX+"px";
+				div.style.top = e.pageY+"px";
+			});
 
 });

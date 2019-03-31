5b7a5f89b39dda879d2bc6b7073138129bb79221
hannah barker <hannah.m.barker@gmail.com>
2019-03-31 14:15:19 -0400

    forced to update, will revise

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 //document ready jquery
 $(() => {
+    //variables declared/initialized 
     let totalSeconds = 0;
-    let totalMinutes =0;
-    let interval, cardOne, cardTwo, selectedCardOne, selectedCardTwo;
+    let totalMinutes = 0;
+    let interval, cardOne, cardTwo, selectedCardOne, selectedCardTwo, timeout;
     let clickCount = 0;
     let countDown = 5;
-   
+    let flag = false;
+    let cardsMatched = 0;
+    let gameWon = false;
 
-    //sets timer in the DOM
+    //
+    // $(`.flip-card-inner`).on(`click`, (e) => {
+    //     var audio = document.getElementById("audio");
+    //     audio.play();
+    // });
+
+
+    //displays timer in the DOM
     function domTimer() {
-        // $(`.seconds`).text(`Seconds: ${totalSeconds}`);
-        // $(`.minutes`).text(`Minutes: ${totalMinutes}`);
         $(`.timer`).text(`Timer: ${totalMinutes}:${totalSeconds}`);
     };
 
-    //sets countdown timer in DOM
+    //displays the count down timer in DOM
     function domCountDown() {
         $(`.timer`).text(`Game Begins in: ${countDown} Seconds`);
     }
 
     //Keeps track of time it takes user to complete game
+    //called in the delayStart function to start clock
     function setTime() {
         //increments timer
         ++totalSeconds;
@@ -28,63 +37,92 @@ $(() => {
             totalMinutes += 1;
             totalSeconds = 0;
         }
-        domTimer();  
+
+        //stops timer when game is won
+        if (gameWon === true) {
+            return;
+        }
+        //calls function to display timer in DOM
+        domTimer();
     };
 
     //function to start game after 30 seconds- gives user time to memorize cards before they flip
     function delayStart() {
-        console.log(`Start Game`);
+        flag = true;
+        clickCount = 0;
+
+        //event listener to flip card animation on click
+        $('.flip-card').click(function () {
+            $(this).toggleClass('active');
+        });
+
         //start in-game timer
         interval = setInterval(setTime, 1000);
         //flip cards back to hidden
         flipCards($('.flip-card'));
-
     };
 
-    //displays the count down timer before game actually starts
-    function delayTimer() {
+    //function to decrement count down timer
+    function countDownTimer() {
         if (countDown != 0) {
             --countDown;
-            // console.log(`Count Down: ${countDown}`);
-            
         } else {
+            //when timer hits 0, exits function (avoids infinite counting)
             return;
-        }domCountDown();
+        }
+        //calls function to insert countdown timer into DOM
+        domCountDown();
     };
 
-    //compares cards based on the src, finds correctly matching cards, **need way to stop user from selecting same card twice** 
+    //compares cards based on the src, finds correctly matching cards
     function compareCards(e) {
         //track number of clicks
         ++clickCount;
         //limit click count to 2
-        while(clickCount > 2) {
+        while (clickCount > 2) {
             clickCount = 0;
             ++clickCount;
         }
-        // console.log(clickCount);
-        if(clickCount === 1) {
-            cardOne = e.currentTarget.children[2].lastElementChild.src;
-            console.log(cardOne);
-            console.log(e.delegateTarget.offsetParent);
-            selectedCardOne = e.delegateTarget.offsetParent;
-        } else {
-            
-            cardTwo = e.currentTarget.children[2].lastElementChild.src;
-            selectedCardTwo = e.delegateTarget.offsetParent;
-            console.log(cardTwo);
-        }
 
-        if( selectedCardOne != selectedCardTwo) {
-            if(clickCount === 2) {
-                if(cardOne === cardTwo) {
-                    console.log(`Match!`);
+        //flag checks if game has started
+        if (flag === true) {
+
+            //sets the first selected card to variable
+            if (clickCount === 1) {
+                cardOne = e.currentTarget.children[2].lastElementChild.src;
+                selectedCardOne = e.delegateTarget.offsetParent;
+            //sets the second selected card to variable
+            } else {
+                cardTwo = e.currentTarget.children[2].lastElementChild.src;
+                selectedCardTwo = e.delegateTarget.offsetParent;
+                selectedCardTwoAccurate = e.delegateTarget.offsetParent.className;
+
+            }
+            //checks cards for match, mismatch, or duplicate selection
+            if (clickCount === 2) {
+                if (selectedCardOne != selectedCardTwo) {
+                    if (cardOne === cardTwo) {
+                        cardsMatched++;
+                    } else {
+                        //flips cards back to hidden after 1 second delay
+                        setTimeout(() => {
+                            flipCards(selectedCardOne);
+                            flipCards(selectedCardTwo);
+                        }, 1000);
+                    }
                 } else {
-                    console.log(`Mismatch!`);
+                    alert(`Can't select the same card!`);
                 }
             }
-    }   else {
-        return;
-    }
+
+        } else {
+            console.log(`game has not started`);
+        }
+        //Check to see if the user has won the game
+        if (cardsMatched === 5) {
+            gameWon = true;
+            console.log("You won the game!")
+        }
     };
 
     //function to flip all cards at beginning
@@ -92,38 +130,7 @@ $(() => {
         $(target).toggleClass('active');
     };
 
-    function shuffle(array) {
-        let length = array.length;
-        let random;
-        let index;
     
-    // While there are elements in the array
-        while (length > 0) {
-    // Pick a random index
-            index = Math.floor(Math.random() * length);
-    // Decrease length by 1
-            length--;
-    // Swap the array elements 
-            random = array[length];
-            array[length] = array[index];
-            array[index] = random;
-        }
-        //Return a random element
-        return random;
-    }
-
-    //event listener to flip card animation on click
-    $('.flip-card').click(function() {
-        $(this).toggleClass('active');
-    });
-
-
-   
-// this is the function for the pop up & 
-// to decide either to be lightside or darkside
-// starts game
-$(document).ready(() => {    
-
     var id = '#dialog';
 
     //Get the window height and width
@@ -157,7 +164,12 @@ $('#lightside').click(function (e) {
         $('.Reset').css('display', 'block');
 
         flipCards($('.flip-card'));
-
+        //prevents user from selecting cards before game starts
+        $(`.flip-card`).unbind(`click`);
+        //disables start button while game is running
+        $(`#Start`).attr("disabled", true);
+        //enables reset button
+        $(`#Reset`).attr("disabled", false);
 });		
 	
 
@@ -178,57 +190,83 @@ $('#darkside').click(function (e) {
         $('.Reset').css('display', 'block');
 
         flipCards($('.flip-card'));
+        //prevents user from selecting cards before game starts
+        $(`.flip-card`).unbind(`click`);
+        //disables start button while game is running
+        $(`#Start`).attr("disabled", true);
+        //enables reset button
+        $(`#Reset`).attr("disabled", false);
         
 
 });	
 
 	
 
-});
 
 
-
-    //reset game
-    $(`#Reset`).on(`click`, (e) => {
-        console.log(`Game Reset`);
-        //pause timer
-        clearInterval(interval);
-        //reset timer
-        totalSeconds = 0;
-        totalMinutes = 0; 
-        //reset timer in dom
-        domTimer();  
-    });
-
-    //event listener for user card selection
-    $(`.flip-card-inner`).on(`click`, (e) => {
-        //compare cards on click
-        compareCards(e);
-        //plays audio on click
-        var audio = document.getElementById("audio");
-        audio.play();
-        // console.log(e);
-    });
-
-
-  
-  
+ 
 // this is the curser for the millenium falcon
 var div = document.getElementById('falcon');
 			document.addEventListener('mousemove',function(e) {			
 				div.style.left = e.pageX+"px";
 				div.style.top = e.pageY+"px";
             });
-            
+       
+   
 
+    //reset game event listener
+    $(`#Reset`).on(`click`, (e) => {
+        //pause timer
+        clearInterval(interval);
+        //pause countdown
+        clearInterval(timeout);
+        //reset timer
+        totalSeconds = 0;
+        totalMinutes = 0;
+        //reset timer in dom
+        domTimer();
+        //flip the cards again
+        flipCards($(`.flip-card`));
+        //reset count down
+        countDown = 5;
+        //re=displays countdown in dom
+        domCountDown();
+        //enables start button
+        $(`#Start`).attr("disabled", false);
+        //disableds reset button
+        $(`#Reset`).attr("disabled", true);
+    });
+    //disableds reset button
+    $(`#Reset`).attr("disabled", true);
+
+    //event listener for user card selection
+    $(`.flip-card-inner`).on(`click`, (e) => {
+        //compare cards on click
+        compareCards(e);
+
+        //plays audio on click
+        // var audio = document.getElementById("audio");
+        // audio.play();
+    });
 
 });
 
+    // function shuffle(array) {
+    //     let length = array.length;
+    //     let random;
+    //     let index;
 
-
-
-// $(document).ready(function(){
-//     setTimeout(function(){
-//        PopUp();
-//     },5000); // 5000 to load it after 5 seconds from page load
-    // });
+    // // While there are elements in the array
+    //     while (length > 0) {
+    // // Pick a random index
+    //         index = Math.floor(Math.random() * length);
+    // // Decrease length by 1
+    //         length--;
+    // // Swap the array elements 
+    //         random = array[length];
+    //         array[length] = array[index];
+    //         array[index] = random;
+    //     }
+    //     //Return a random element
+    //     return random;
+    // }
